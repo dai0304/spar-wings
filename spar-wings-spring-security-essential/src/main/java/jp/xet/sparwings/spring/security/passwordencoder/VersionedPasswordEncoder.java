3d6a783157124d0745bfdb6b32707befb31fce53
Daisuke Miyamoto <dai.0304@gmail.com>
2016-03-20 11:01:19 +0900

    Add VersionedPasswordEncoder

---
+++ spar-wings-spring-security-essential/src/main/java/jp/xet/sparwings/spring/security/passwordencoder/VersionedPasswordEncoder.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2015-2016 Miyamoto Daisuke.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.xet.sparwings.spring.security.passwordencoder;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.util.Assert;
+
+/**
+ * TODO for daisuke
+ * 
+ * @author daisuke
+ * @since #version#
+ */
+public class VersionedPasswordEncoder implements PasswordEncoder {
+	
+	private static Logger logger = LoggerFactory.getLogger(VersionedPasswordEncoder.class);
+	
+	private static final Pattern VERSION_KEY_PATTERN = Pattern.compile("\\A[0-9A-Za-z]+");
+	
+	private static final Pattern VERSION_ENCODED_PATTERN = Pattern.compile("\\A([0-9A-Za-z]+)_(.*)");
+	
+	private static final Pattern BCRYPT_PATTERN = Pattern.compile("\\A\\$2a?\\$\\d\\d\\$[./0-9A-Za-z]{53}");
+	
+	private static final PasswordEncoder BCRYPT = new BCryptPasswordEncoder();
+	
+	private static final Pattern STANDARD_PATTERN = Pattern.compile("\\A[0-9A-Fa-f]+");
+	
+	private static final PasswordEncoder STANDARD = new StandardPasswordEncoder();
+	
+	private static final Map<String, PasswordEncoder> DEFAULT_ENCODERS;
+	static {
+		Map<String, PasswordEncoder> map = new HashMap<>();
+		map.put("NoOp", NoOpPasswordEncoder.getInstance());
+		map.put("Std", STANDARD);
+		map.put("BC", BCRYPT);
+		DEFAULT_ENCODERS = Collections.unmodifiableMap(map);
+	}
+	
+	private final Map<String, PasswordEncoder> encoders;
+	
+	private final String primaryEncoder;
+	
+	private final String unknownEncoder;
+	
+	@Getter
+	@Setter
+	private boolean enableAutoDetection = true;
+	
+	
+	/**
+	 * Create instance with default encoder map.
+	 * 
+	 * @since #version#
+	 */
+	public VersionedPasswordEncoder() {
+		this("NoOp", "BC", DEFAULT_ENCODERS);
+	}
+	
+	/**
+	 * Create instance with custom encoder map.
+	 * 
+	 * @param unknownEncoder The map key for unknwon encoder
+	 * @param primaryEncoder The map key for primary encoder
+	 * @param encoders The map of {@link PasswordEncoder}
+	 * @since 0.9
+	 */
+	public VersionedPasswordEncoder(String unknownEncoder, String primaryEncoder, Map<String, PasswordEncoder> encoders) {
+		Assert.notNull(encoders);
+		Assert.isTrue(encoders.containsKey(unknownEncoder), "The encoders must to contain unknown-encoder");
+		Assert.isTrue(encoders.containsKey(primaryEncoder), "The encoders must to contain primary-encoder");
+		if (encoders.keySet().stream().allMatch(key -> VERSION_KEY_PATTERN.matcher(key).matches()) == false) {
+			throw new IllegalArgumentException("Some keys unmatches the key pattern.");
+		}
+		
+		this.primaryEncoder = primaryEncoder;
+		this.unknownEncoder = unknownEncoder;
+		this.encoders = Collections.unmodifiableMap(encoders);
+	}
+	
+	@Override
+	public String encode(CharSequence rawPassword) {
+		String encoded = encoders.get(primaryEncoder).encode(rawPassword);
+		return String.format("%s_%s", primaryEncoder, encoded);
+	}
+	
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		try {
+			Matcher matcher = VERSION_ENCODED_PATTERN.matcher(encodedPassword);
+			if (matcher.matches()) {
+				String key = matcher.group(1);
+				String body = matcher.group(2);
+				
+				PasswordEncoder encoder;
+				if (encoders.containsKey(key)) {
+					encoder = encoders.get(key);
+				} else {
+					logger.warn("Unknown encoding type: {} ... using unknown encoder {}", key, unknownEncoder);
+					encoder = encoders.get(unknownEncoder);
+				}
+				
+				return encoder.matches(rawPassword, body);
+			} else if (enableAutoDetection) {
+				if (BCRYPT_PATTERN.matcher(encodedPassword).matches()) { // auto detect BCrypt
+					logger.info("It seems to be BCrypt encoded password...");
+					if (BCRYPT.matches(rawPassword, encodedPassword)) {
+						return true;
+					}
+				}
+				if (STANDARD_PATTERN.matcher(encodedPassword).matches()) { // auto detect Standard
+					logger.info("It seems to be Standard encoded password...");
+					if (STANDARD.matches(rawPassword, encodedPassword)) {
+						return true;
+					}
+				}
+			}
+			return encoders.get(unknownEncoder).matches(rawPassword, encodedPassword);
+		} catch (Exception e) {
+			logger.warn("Unexpected", e);
+		}
+		return false;
+	}
+}
---
+++ spar-wings-spring-security-essential/src/test/java/jp/xet/sparwings/spring/security/passwordencoder/VersionedPasswordEncoderTest.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2015-2016 Miyamoto Daisuke.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.xet.sparwings.spring.security.passwordencoder;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+/**
+ * Test for {@link VersionedPasswordEncoder}.
+ * 
+ * @since #version#
+ */
+@SuppressWarnings("javadoc")
+public class VersionedPasswordEncoderTest {
+	
+	Map<String, PasswordEncoder> legalMap = ImmutableMap.<String, PasswordEncoder> builder()
+		.put("foo", NoOpPasswordEncoder.getInstance())
+		.put("bar", new StandardPasswordEncoder())
+		.put("baz", new BCryptPasswordEncoder())
+		.build();
+	
+	Map<String, PasswordEncoder> illegalMap = ImmutableMap.<String, PasswordEncoder> builder()
+		.put("foo-bar", NoOpPasswordEncoder.getInstance())
+		.put("bar", new StandardPasswordEncoder())
+		.build();
+	
+	VersionedPasswordEncoder sut = new VersionedPasswordEncoder("foo", "baz", legalMap);
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstruct_primaryEncoder_notFound() {
+		new VersionedPasswordEncoder("foo", "xxx", legalMap);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstruct_unknownEncoder_notFound() {
+		new VersionedPasswordEncoder("xxx", "bar", legalMap);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstruct_keyPattern_doesNotMet() {
+		new VersionedPasswordEncoder("bar", "foo-bar", illegalMap);
+	}
+	
+	@Test
+	public void testEncode() {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		// exercise
+		String actual = sut.encode(rawPassword);
+		// verify
+		assertThat(actual, startsWith("baz_"));
+	}
+	
+	@Test
+	public void testMatchesEncodedWithKey1() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "baz_$2a$10$cuR2ghEiftvcxKx/V8ZNieZBs4Xfiq6ZsRapxHZRnfWpMyBwQYUme"; // new BCryptPasswordEncoder().encode(rawPassword);
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void testMatchesEncodedWithKey2() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "foo_p@ssW0rd";
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void testMatchesEncodedWithKey3() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "bar_9bf18ae42422e047a5fa740bc9a41110d0146c5cf2b8ba6df1cc583e23202adbd132f9cb3da6e9c1"; // new StandardPasswordEncoder().encode(rawPassword);
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void testMatchesEncodedWithoutKey() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "p@ssW0rd";
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void testMatchesAutoDetection_STANDARD() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "9bf18ae42422e047a5fa740bc9a41110d0146c5cf2b8ba6df1cc583e23202adbd132f9cb3da6e9c1";
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void testMatchesAutoDetection_BCRYPT() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "$2a$10$cuR2ghEiftvcxKx/V8ZNieZBs4Xfiq6ZsRapxHZRnfWpMyBwQYUme";
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void testMatchesAutoDetectionDisabled_STANDARD() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "9bf18ae42422e047a5fa740bc9a41110d0146c5cf2b8ba6df1cc583e23202adbd132f9cb3da6e9c1";
+		sut.setEnableAutoDetection(false);
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void testMatchesAutoDetectionDisabled_BCRYPT() throws Exception {
+		// setup
+		String rawPassword = "p@ssW0rd";
+		String encodedPassword = "$2a$10$cuR2ghEiftvcxKx/V8ZNieZBs4Xfiq6ZsRapxHZRnfWpMyBwQYUme";
+		sut.setEnableAutoDetection(false);
+		// exercise
+		boolean actual = sut.matches(rawPassword, encodedPassword);
+		// verify
+		assertFalse(actual);
+	}
+}
