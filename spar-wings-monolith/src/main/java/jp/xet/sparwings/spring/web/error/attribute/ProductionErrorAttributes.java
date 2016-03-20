86cdda4f186d66a38cc65a22b4d236457709e1e0
Daisuke Miyamoto <dai.0304@gmail.com>
2016-03-20 10:39:58 +0900

    Cosmetic change

--- spar-wings-monolith/src/main/java/jp/xet/sparwings/spring/web/error/attribute/ProductionErrorAttributes.java
+++ spar-wings-monolith/src/main/java/jp/xet/sparwings/spring/web/error/attribute/ProductionErrorAttributes.java
@@ -59,8 +59,7 @@ public class ProductionErrorAttributes implements ErrorAttributes {
 	
 	private void addStatus(Map<String, Object> errorAttributes,
 			RequestAttributes requestAttributes) {
-		Integer status = getAttribute(requestAttributes,
-				"javax.servlet.error.status_code");
+		Integer status = getAttribute(requestAttributes, "javax.servlet.error.status_code");
 		if (status == null) {
 			errorAttributes.put("status", 999);
 			errorAttributes.put("error", "None");
@@ -79,8 +78,8 @@ public class ProductionErrorAttributes implements ErrorAttributes {
 			RequestAttributes requestAttributes, boolean includeStackTrace) {
 		Throwable error = getError(requestAttributes);
 		Object message = getAttribute(requestAttributes, "javax.servlet.error.message");
-		if ((!StringUtils.isEmpty(message) || errorAttributes.get("message") == null)
-				&& !(error instanceof BindingResult)) {
+		if ((StringUtils.isEmpty(message) == false || errorAttributes.get("message") == null)
+				&& (error instanceof BindingResult) == false) {
 			errorAttributes.put("message", StringUtils.isEmpty(message) ? "No message available" : message);
 		}
 	}
