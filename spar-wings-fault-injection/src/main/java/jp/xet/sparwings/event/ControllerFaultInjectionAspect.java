8e9bd00aca67d8649e1999eee25ab54f18fe0674
Daisuke Miyamoto <dai.0304@gmail.com>
2016-04-22 14:21:51 +0900

    Add fault-injection

---
+++ spar-wings-fault-injection/src/main/java/jp/xet/sparwings/event/ControllerFaultInjectionAspect.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2011 Daisuke Miyamoto. (http://d.hatena.ne.jp/daisuke-m)
+ * Created on 2016/04/21
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.xet.sparwings.event;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Aspect for fault injection to Spring MVC controllers.
+ * 
+ * @since #version#
+ * @author daisuke
+ */
+@Aspect
+public class ControllerFaultInjectionAspect extends AbstractFaultInjectionAspect {
+	
+	private static final Map<String, Supplier<RuntimeException>> SUPPLIERS;
+	static {
+		Map<String, Supplier<RuntimeException>> supplisers = new HashMap<>();
+		supplisers.put("controller:FaultInjectionException", () -> new FaultInjectionException("fault injected"));
+		SUPPLIERS = Collections.unmodifiableMap(supplisers);
+	}
+	
+	
+	/**
+	 * インスタンスを生成する。
+	 */
+	public ControllerFaultInjectionAspect() {
+		super(SUPPLIERS);
+	}
+	
+	@Pointcut("("
+			+ "  within(@org.springframework.stereotype.Controller *)"
+			+ "  || within(@org.springframework.web.bind.annotation.RestController *)"
+			+ ")"
+			+ " && @annotation(requestMapping)"
+			+ " && !execution(* org.springframework.boot.autoconfigure.web.BasicErrorController.*(..))")
+	public void controller(RequestMapping requestMapping) {
+	}
+	
+	/**
+	 * TODO for daisuke
+	 * 
+	 * @param joinPoint
+	 * @param requestMapping
+	 * @return
+	 * @throws Throwable
+	 */
+	@Around("controller(requestMapping)")
+	public Object faultInjectionAdvice(ProceedingJoinPoint joinPoint, RequestMapping requestMapping) throws Throwable {
+		return super.faultInjectionAdvice(joinPoint);
+	}
+}
---
+++ spar-wings-fault-injection/src/main/java/jp/xet/sparwings/event/DynamoDBFaultInjectionAspect.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2011 Daisuke Miyamoto. (http://d.hatena.ne.jp/daisuke-m)
+ * Created on 2016/04/21
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.xet.sparwings.event;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputExceededException;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * Aspect for fault injection to AWS DynamoDB cluent.
+ * 
+ * @since #version#
+ * @author daisuke
+ */
+@Aspect
+public class DynamoDBFaultInjectionAspect extends AbstractFaultInjectionAspect {
+	
+	private static final Map<String, Supplier<RuntimeException>> SUPPLIERS;
+	static {
+		Map<String, Supplier<RuntimeException>> supplisers = new HashMap<>();
+		supplisers.put("dynamodb:ProvisionedThroughputExceededException",
+				() -> new ProvisionedThroughputExceededException("fault injected"));
+		SUPPLIERS = Collections.unmodifiableMap(supplisers);
+	}
+	
+	
+	/**
+	 * インスタンスを生成する。
+	 */
+	public DynamoDBFaultInjectionAspect() {
+		super(SUPPLIERS);
+	}
+	
+	/**
+	 * TODO for daisuke
+	 * 
+	 * @param joinPoint
+	 * @return
+	 * @throws Throwable
+	 */
+	@Override
+	@Around("execution(* com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient.*(..))")
+	public Object faultInjectionAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+		return super.faultInjectionAdvice(joinPoint);
+	}
+}
