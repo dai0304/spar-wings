660043c8ad7cccd65011146476343427cecc8102
Daisuke Miyamoto <dai.0304@gmail.com>
2016-11-25 18:18:56 +0900

    Always use default page size if invalid or unspecified size

--- spar-wings-spring-data-chunk/src/main/java/jp/xet/sparwings/spring/data/web/ChunkableHandlerMethodArgumentResolver.java
+++ spar-wings-spring-data-chunk/src/main/java/jp/xet/sparwings/spring/data/web/ChunkableHandlerMethodArgumentResolver.java
@@ -22,6 +22,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.MethodParameter;
@@ -45,6 +46,7 @@ import jp.xet.sparwings.spring.data.chunk.Chunkable.PaginationRelation;
  * @since 0.19
  * @author daisuke
  */
+@Slf4j
 public class ChunkableHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver { // NOPMD - cc
 	
 	private static final String INVALID_DEFAULT_PAGE_SIZE =
@@ -64,7 +66,7 @@ public class ChunkableHandlerMethodArgumentResolver implements HandlerMethodArgu
 	
 	private static final int DEFAULT_MAX_PAGE_SIZE = 2000;
 	
-	static final Chunkable DEFAULT_CHUNK_REQUEST = new ChunkRequest(null, null, null, null);
+	static final Chunkable DEFAULT_CHUNK_REQUEST = new ChunkRequest(null, null, DEFAULT_MAX_PAGE_SIZE, null);
 	
 	
 	private static Chunkable getDefaultChunkRequestFrom(MethodParameter parameter) {
@@ -166,25 +168,19 @@ public class ChunkableHandlerMethodArgumentResolver implements HandlerMethodArgu
 			return defaultOrFallback;
 		}
 		
-		Integer pageSize;
+		Integer pageSize = maxPageSize;
 		if (StringUtils.hasText(pageSizeString)) {
 			try {
-				int parsed = Integer.parseInt(pageSizeString);
-				pageSize = parsed < 0 ? 0 : parsed > maxPageSize ? maxPageSize : parsed;
+				pageSize = Integer.parseInt(pageSizeString);
 			} catch (NumberFormatException e) {
-				pageSize = null;
+				log.trace("invalid page size: {}", pageSizeString);
 			}
-		} else {
-			pageSize = defaultOrFallback.getMaxPageSize();
 		}
-		if (pageSize != null) {
-			// Limit lower bound
-			pageSize = pageSize < 1 ? 1 : pageSize;
-		}
-		if (pageSize != null) {
-			// Limit upper bound
-			pageSize = pageSize > maxPageSize ? maxPageSize : pageSize;
-		}
+		
+		// Limit lower bound
+		pageSize = pageSize < 1 ? 1 : pageSize;
+		// Limit upper bound
+		pageSize = pageSize > maxPageSize ? maxPageSize : pageSize;
 		
 		Direction direction = Direction.fromStringOrNull(directionString);
 		
