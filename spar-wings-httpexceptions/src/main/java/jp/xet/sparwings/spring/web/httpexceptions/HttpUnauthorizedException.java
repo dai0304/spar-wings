f0e42d5a34ebc697167f52c83c0134e0e0dac9f5
Daisuke Miyamoto <dai.0304@gmail.com>
2017-06-23 12:13:30 +0900

    Add 401 exception

---
+++ spar-wings-httpexceptions/src/main/java/jp/xet/sparwings/spring/web/httpexceptions/HttpUnauthorizedException.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.xet.sparwings.spring.web.httpexceptions;
+
+import lombok.NoArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Respond 401.
+ * 
+ * @since #version#
+ * @author daisuke
+ */
+@NoArgsConstructor
+@SuppressWarnings("serial")
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class HttpUnauthorizedException extends HttpResponseException {
+	
+	/**
+	 * インスタンスを生成する。
+	 * 
+	 * @param message 例外メッセージ
+	 * @param cause 起因例外
+	 * @since #version#
+	 */
+	public HttpUnauthorizedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 * 
+	 * @param message 例外メッセージ
+	 * @since #version#
+	 */
+	public HttpUnauthorizedException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 * 
+	 * @param cause 起因例外
+	 * @since #version#
+	 */
+	public HttpUnauthorizedException(Throwable cause) {
+		super(cause);
+	}
+}
