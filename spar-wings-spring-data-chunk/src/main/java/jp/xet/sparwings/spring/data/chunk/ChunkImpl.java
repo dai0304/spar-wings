9c04a571154b2158e1662d509624ba79040cd8dc
wreulicke <saito.masaya@classmethod.jp>
2018-07-20 15:57:09 +0900

    fix format

--- spar-wings-spring-data-chunk/src/main/java/jp/xet/sparwings/spring/data/chunk/ChunkImpl.java
+++ spar-wings-spring-data-chunk/src/main/java/jp/xet/sparwings/spring/data/chunk/ChunkImpl.java
@@ -16,7 +16,6 @@
 package jp.xet.sparwings.spring.data.chunk;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
